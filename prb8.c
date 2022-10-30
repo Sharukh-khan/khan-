5818812a40d1766d1e7e7364fabc605160d6b134
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-10-30 20:46:54 +0530

    Update prb8.c

--- prb8.c
+++ prb8.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-int a, b ,c ;
+int a, b ,c,d ;
 scanf("%d",&a); //getting integer from user 
-scanf("%d",&b);  //getting bit position from user
-for(b;b<=31;b++){ // for loop with condition from 0 to 31 
-  c=(1<<b)&a;    // shfting 1 integer with according to bit positon number and using AND operator
+b=sizeof(a)*8; // getting bits of number a
+for(d=0;d<=b-1;d++){ // for loop with condition from 0 to 31 
+  c=(1<<d)&a;    // shfting 1 integer with according to d and using AND operator
        if(c!=0){                         // if c is not equal to zero
       printf("the lowest bit :%d",c);   //lowest bit
   break;
