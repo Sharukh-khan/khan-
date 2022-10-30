95814e05bb1ec7b469191034cb3f92f7afe18c84
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-10-30 21:07:22 +0530

    Update prb7.c

--- prb7.c
+++ prb7.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a,b,c,d;
+    int a,b,c,d,e;
     scanf("%d",&a);//getting integer from user 
-    scanf("%d",&b); //getting bit position from user
-     for(b=b;b<=31;b++){   // for loop with condition from 0 to 31 
-         c=(1<<b)&a;       // shfting 1 integer with according to bit positon number and using AND operator
+     b= sizeof(a)*8 ;//getting bit of a
+     for(e=0;e<b-1;e++){   // for loop with condition from 0 to 31 
+         c=(1<<e)&a;       // shfting 1 integer with according to bit positon number and using AND operator
          if(c!=0){    // if c is not equal to zero
              d=c;     // assining d as highest bit number
          }
