c4fc90be76acbfd61b29fcd8e948fa8aa2b2d030
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-11-02 19:15:35 +0530

    Update prb9.c

--- prb9.c
+++ prb9.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-  int a,b,c,d=0,e;
-  scanf("%d",&a);  //getting integer from user
-  b=sizeof(a)*8;   // getting bits of a 
-  for(e=0;e<b-1;e++){        // for loop with condition from e =0 to  up to b
-   c=(1<<e)&a;         // shfting 1 integer with according to e and using AND operator
-   if( c==0){                // if c is  equal to zero
+  int a,b,c,d=0,e;                                                                                                    //step1
+  scanf("%d",&a);  //getting integer from user                                                                        //step2
+  b=sizeof(a)*8;   // getting bits of a                                                                               //steps3
+  for(e=0;e<b-1;e++){        // for loop with condition from e =0 to  up to b                                         //step4
+   c=(1<<e)&a;         // shfting 1 integer with according to e and using AND operator                                
+   if( c==0){                // if c is  equal to zero                                                                //step5
      d++;
      continue;
      }else{
          break;
      }
      }
-     printf("the trailing zeros are : %d",d);   //trailing  zero
+     printf("the trailing zeros are : %d",d);   //trailing  zero                                                      //step6
      }
      /*   algoritham
 step1:  declaring a variable  a,b,c,d=0,e
