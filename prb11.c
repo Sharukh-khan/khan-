d81858d27f1cba689f6942326f36949edb8afa6a
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-11-02 18:56:04 +0530

    Delete prb11.c

--- prb11.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
- int a,b; //declaring variables a,b
-  scanf("%d",&a);  //getting input from user
- b=a^(a|(~a));  //(1.inverting a ) (2.then using OR operator ) (3.and Then XOR)
-  printf("%d",b); //PRINTING VALUE with negative sign 
-}
