b27b7ccffd69da7e4bc6753e99ffb3a450b8f03c
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-11-02 19:20:20 +0530

    Update prb12.c

--- prb12.c
+++ prb12.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main(){
-int a,b,c,d,zeros=0,one=0; //declaring variables
-scanf("%d",&a);  //getting input from user
-b=sizeof(a)*8;   //getting bits of a number
-for(c=0;c<b-1;c++){  //running loop from 0 to b
-d=(1<<c)&a;  //shifting 1 as with shift operator and using AND opertor
-if(d==0){  //if d is zero
+int a,b,c,d,zeros=0,one=0; //declaring variables                             //step1
+scanf("%d",&a);  //getting input from user                                   //step2
+b=sizeof(a)*8;   //getting bits of a number                                  //step3
+for(c=0;c<b-1;c++){  //running loop from 0 to b                              //step4
+d=(1<<c)&a;  //shifting 1 as with shift operator and using AND opertor        
+if(d==0){  //if d is zero                                                     //step5
 zeros++;
 }else{  //if d is other than zero
 one++;
 }
-}printf("zeros: %d and ones: %d",zeros,one);
+}printf("zeros: %d and ones: %d",zeros,one);                                //step6
 return 0;
 }
 
