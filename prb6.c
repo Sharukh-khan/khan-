356820a1680b0f08723124ccef92b137e4e1c553
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-11-02 19:08:35 +0530

    Update prb6.c

--- prb6.c
+++ prb6.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a,b;
-    scanf("%d",&a);//getting integer from user 
-    scanf("%d",&b);//getting bit position from user
-    a=(1<<b)^a;    // shifting 1 number with bit position number with XOR operator (( toggle to change 1 to 0 and 0 to 1))
-    printf("the output:%d",a);
+    int a,b;                                                                                                                          //step 1
+    scanf("%d",&a);//getting integer from user                                                                                 //step2              
+    scanf("%d",&b);//getting bit position from user                                                                                 //step3
+    a=(1<<b)^a;    // shifting 1 number with bit position number with XOR operator (( toggle to change 1 to 0 and 0 to 1))        //step4
+    printf("the output:%d",a);                                                                                                       //step5
 }
 
 /*algoritham
