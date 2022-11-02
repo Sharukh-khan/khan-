d28dafdd3acc73ee5e50202e5047122652dfc94c
Sharukh-khan <116792013+Sharukh-khan@users.noreply.github.com>
2022-11-02 18:57:18 +0530

    Delete prb4.c

--- prb4.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    scanf("%d",&a); //getting integer from user 
-    scanf("%d",&b); //getting bit position from user
-    a=(1<<b)|a;  //shifting 1 number with shift operator as with bit position number and OR operator 
-    printf("the output is : %d",a); // output ater set  
-    return 0;
-}
